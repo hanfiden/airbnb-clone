0db1b0f359265468058540d837b2a63060f4f605
Han Fiden <fidendu2@gmail.com>
2022-03-16 14:51:04 +0100

    Add footer

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It a pretty clone of Airbnb</p>
+        <p>My name is Fiden</p>
+        <p>How are you doing?</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Want a drink?</p>
+        <p>Call me</p>
+        <p>I live in Nantes</p>
+        <p>Nantes is a good city</p>
+        <p>Love it</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Be kind</p>
+        <p>Be nice</p>
+        <p>Be supportive</p>
+        <p>Keep cool</p>
+        <p>Love and peace</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Banner from '../components/Banner'
 import SmallCard from '../components/SmallCard'
 import MediumCard from '../components/MediumCard'
 import LargeCard from '../components/LargeCard'
+import Footer from '../components/Footer'
 
 export default function Home({ exploreData, liveData }) {
   return (
@@ -58,6 +59,8 @@ export default function Home({ exploreData, liveData }) {
           />
         </section>
       </main>
+
+      <Footer />
     </div>
   )
 }
